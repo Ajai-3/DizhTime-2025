997aeecc8b207675c28f4025d21a8eb22651e899
Ajai-3 <ajaipjayan@gmail.com>
2025-06-15 13:43:29 +0530

    chore: update package dependencies and configuration files; add next-themes package

--- services/delivery-boy-service/src/app.ts
+++ services/delivery-boy-service/src/app.ts
@@ -3,8 +3,11 @@ import express from 'express';
 import dotenv from 'dotenv';
 import deliveryBoyRoutes from './routes/deliveryBoy.route';
 dotenv.config();
+
 const app = express();
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
 
 app.use('/api', deliveryBoyRoutes);
 
--- services/delivery-boy-service/src/server.ts
+++ services/delivery-boy-service/src/server.ts
@@ -1,16 +1,16 @@
-import http from 'http';
-import mongoose from 'mongoose';
-import app from './app';
-import dotenv from 'dotenv';
+import http from "http";
+import app from "./app";
+import dotenv from "dotenv";
+import { connectDB } from "./config/database.config";
+
 dotenv.config();
 
 const PORT = process.env.PORT || 3003;
 const server = http.createServer(app);
 
+// Connect to database
+connectDB();
 
-mongoose.connect(process.env.DATABASE_URL!)
-    .then(() => {
-        console.log('MongoDB connected');
-        server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-    })
-    .catch((error) => console.error('MongoDB connection error:', error));
+server.listen(PORT, () => {
+  console.log(`Delivery Boy Service running on port ${PORT}`);
+});
