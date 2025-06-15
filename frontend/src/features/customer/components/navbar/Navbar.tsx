521a9262daea9dbbc861c6be92743cee6cec6521
Ajai-3 <ajaipjayan@gmail.com>
2025-06-15 18:46:39 +0530

    KAN-9 feat: enhance Navbar with user authentication buttons and improve ThemeSwitcher styling

--- frontend/src/features/customer/components/navbar/Navbar.tsx
+++ frontend/src/features/customer/components/navbar/Navbar.tsx
@@ -1,20 +1,40 @@
-import React from 'react'
-import NavLogo from './NavLogo'
-import NavLinks from './NavLinks'
-import NavAuthButtons from './NavAuthButtons'
-import NavUserSection from './NavUserSection'
-import ThemeSwitcher from '../../../../components/ThemeSwitcher'
+import React from "react";
+import NavLogo from "./NavLogo";
+import NavLinks from "./NavLinks";
+import NavAuthButtons from "./NavAuthButtons";
+import NavUserSection from "./NavUserSection";
+import ThemeSwitcher from "../../../../components/ThemeSwitcher";
 
-const Navbar:React.FC = () => {
+const Navbar: React.FC = () => {
+  let user: boolean = true;
+
+  const dummyUser = {
+    name: "Clara lagoooosssssssss",
+    image: "https://i.pravatar.cc/150?img=3",
+  };
+
   return (
-    <div className='px-4 md:px-20 lg:px-34 py-4 flex justify-between items-center'>
-       <NavLogo />
-       <NavLinks />
-       <NavAuthButtons />
-       <NavUserSection />
-       <ThemeSwitcher />
-    </div>
-  )
-}
+    <div className="px-4 md:px-10 lg:px-36 py-4 flex justify-between items-center">
+      <NavLogo />
+      <NavLinks />
 
-export default Navbar
+      {user ? (
+        <div className="flex gap-2">
+          {/* <div className="hidden sm:block">
+            <ThemeSwitcher />
+          </div> */}
+          <NavUserSection user={dummyUser} />
+        </div>
+      ) : (
+        <div className="flex gap-2">
+          {/* <div className="hidden sm:block">
+            <ThemeSwitcher />
+          </div> */}
+          <NavAuthButtons />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Navbar;
